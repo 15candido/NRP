8a312abe6d2e95bd5d28197e7f6c0f8a913b95a3
dvdfreitas <dvdfreitas@gmail.com>
2022-04-11 12:22:06 +0100

    2204111221 - Navbar

--- resources/views/components/navbar/navbar.blade.php
+++ resources/views/components/navbar/navbar.blade.php
@@ -1,5 +1,15 @@
-<div class="flex bg-yellow-500 py-4 ">
-    <x-navbar.button href="https://www.google.pt" ligado="true">Homepage</x-navbar.button>
-    <x-navbar.button href="/quem_somos">Quem somos</x-navbar.button>
-    <x-navbar.button href="/projetos">Projetos</x-navbar.button>
+<div class="max-w-7xl m-auto">
+    <div class="flex justify-end bg-red-400 w-full">
+        <div>Login</div>
+    </div>
+    <div class="flex">
+        <div>
+            <img src="img/logo.jpg" class="h-32">
+        </div>
+        <div class="flex justify-end w-full">
+            <x-navbar.button href="https://www.google.pt" ligado="true">Homepage</x-navbar.button>
+            <x-navbar.button href="/quem_somos">Quem somos</x-navbar.button>
+            <x-navbar.button href="/projetos">Projetos</x-navbar.button>
+        </div>
+    </div>
 </div>
