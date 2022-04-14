51c322c310c727af53a10e965b799c97b1f4e3a5
candido <candido.da.silva.ca@gmail.com>
2022-04-14 16:17:07 +0100

    Resposividade

--- resources/views/components/navbar/button.blade.php
+++ resources/views/components/navbar/button.blade.php
@@ -1,6 +1,6 @@
 @props(['ligado' => false, 'href' => '#'])
 
-<li class="mx-4  rounded-full px-1">
+<li class="mx-4  my-6 sm:my-0">
     <a href="{{ $href }}" class="hover:text-cyan-500 duration-500 ">
         {{ $slot }}
     </a>
