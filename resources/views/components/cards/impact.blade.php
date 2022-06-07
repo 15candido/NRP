79c3d465614a3962128ef8b2fb9f098dc26bd0da
candido <candido.da.silva.ca@gmail.com>
2022-06-07 15:45:44 +0100

    Resolver conflit

--- resources/views/components/cards/impact.blade.php
+++ resources/views/components/cards/impact.blade.php
@@ -1,6 +1,20 @@
-<div class="grid grid-cols-1 md:grid-cols-2 gap-4 resize-w h-full absolute px-2 py-4">
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4 resize-w h-full absolute px-2 py-8">
   <div class="overflow-hidden">
-    
+    <p>Tailwind lets you conditionally apply utility classes in different states using variant modifiers.
+      For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.
+    </p>
+    <p>Tailwind lets you conditionally apply utility classes in different states using variant modifiers.
+      For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.
+    </p>
+    <p>Tailwind lets you conditionally apply utility classes in different states using variant modifiers.
+      For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.
+    </p>
+    <p>Tailwind lets you conditionally apply utility classes in different states using variant modifiers.
+      For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.
+    </p>
+    <p>Tailwind lets you conditionally apply utility classes in different states using variant modifiers.
+      For example, use hover:grid-cols-6 to only apply the grid-cols-6 utility on hover.
+    </p>
   </div>
   <div class="w-full max-w-full h-full overflow-hidden"> <!-- Video container of volunteers -->
     <video class="w-full max-w-full h-full object-cover object-center"  autoplay loop muted src="video/ResumoAtividadeVoluntários.mp4"></video>
