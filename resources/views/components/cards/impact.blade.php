6caf7e6da89ff7d62d3bb9220586aac4b1cea5a7
candido <candido.da.silva.ca@gmail.com>
2022-06-08 11:55:39 +0100

    Atualizar css

--- resources/views/components/cards/impact.blade.php
+++ resources/views/components/cards/impact.blade.php
@@ -1,5 +1,5 @@
 <div class="grid grid-cols-1 md:grid-cols-2 gap-4 resize-w h-full absolute px-2 py-8 justify-items-center">
-  <div class="overflow-hidden justify-items-center text-gray-600">
+  <div class="overflow-hidden justify-items-center text-gray-600"> <!-- Results over the years -->
     <x-cards.result/>
   </div>
   <div class="w-full max-w-full h-full overflow-hidden rounded-lg shadow-2xl"> <!-- Video container of volunteers -->
