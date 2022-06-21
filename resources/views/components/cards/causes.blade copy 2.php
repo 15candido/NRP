ef1b5c64a05b2dbbf1084c9778a9a0ceab56c61d
candido <candido.da.silva.ca@gmail.com>
2022-06-21 12:38:50 +0100

    Ajuste no componente

--- resources/views/components/cards/causes.blade copy 2.php
+++ resources/views/components/cards/causes.blade copy 2.php
@@ -1,5 +1,5 @@
 
- <div class="space-y-4 grid grid-cols-1 rounded-lg shadow-2xl bg-white overflow-hidden 2xl:grid-cols-3 2xl:gap-4 2xl:space-y-0 lg:gap-2 ">
+ <div class="bg-black space-y-4 grid grid-cols-1 rounded-lg shadow-2xl overflow-hidden 2xl:grid-cols-3 2xl:gap-4 2xl:space-y-0 lg:gap-2 ">
   <div class="relative h-0 pb-2/3 2xl:pt-1/2 2xl:pb-0">
     <img src="img/ProjectImg/mame.jpeg" alt="A Educação é o Único Caminho" 
     class="absolute inset-0 w-full h-full object-cover shadow-lg rounded-lg">
