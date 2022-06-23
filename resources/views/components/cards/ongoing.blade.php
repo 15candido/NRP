c878fd578ed54de568fd027ad1b527177e5c8a14
candido <candido.da.silva.ca@gmail.com>
2022-06-23 09:24:33 +0100

    Sessão projeto na página homepage/landingpage

---
+++ resources/views/components/cards/ongoing.blade.php
@@ -0,0 +1,74 @@
+
+<div class="relative grid grid-flow-row justify-items-center w-full h-auto lg:resize-h  overflow-hidden
+my-10 bg-gradient-to-r from-[#4ca747] via-[#019393] to-[#0082b3]">
+    <div class="flex flex-col max-w-[91.666667%] w-full h-full justify-center lg:max-w-[83.333333%] gap-4">
+        <div class="flex flex-col h-full justify-center items-center md:grid md:grid-cols-4 gap-2 text-white">
+            <div class="flex flex-col justify-center items-center w-full h-ful">
+                <div class="flex flex-wrap w-full h-full justify-center items-start px-2">
+                    <span class="text-left leading-normal">
+                        <h2 class="pb-4 text-lg font-extrabold">Projetos em Curso </h2>
+                        <p class="text-base">
+                        <span class="text-base font-extrabold text-white/90">Na Rota dos Povos</span>
+                            – Organização não Governamental para o Desenvolvimento criada em 2001. 
+                            Tem a sua sede em Matosinhos. Tudo o que se faz na ONGD é feito por voluntários
+                            (desde a angariação, a gestão do dia-a-dia, a organização de missões, a comunicação, etc).
+                        </p>
+                    </span>
+                </div>
+            </div>
+            <div class="col-span-3 w-full h-full">
+               <div class="flex flex-col w-full h-full justify-center md:flex-row gap-2">
+                    <div class="flex flex-wrap w-full h-full md:w-1/4 shadow-xl">
+                        <div class="w-full h-2/3">
+                            <img class="object-cover w-full h-full" src="img/ProjectImg/educacao.jpg" alt="Shoes" />
+                        </div>
+                        <div class="flex flex-wrap w-full h-1/3 p-1">
+                            <h2 class="card-title text-base text-white/70">A Educação é o Único Caminho</h2>
+                            <!-- <p>A educação é um fator essencial para ...</p> -->
+                            <div class="card-button">
+                               <x-button.read-more>Saber +</x-button.read-more>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap w-full h-full md:w-1/4 shadow-xl">
+                        <div class="w-full h-2/3">
+                            <img class="object-cover w-full h-full" src="img/ProjectImg/mame.jpeg" alt="Casa da Mamé" />
+                        </div>
+                        <div class="flex flex-wrap w-full h-1/3 p-1">
+                            <h2 class="card-title text-base text-white/70">Casa da Mamé - Acolhimento de Crianças</h2>
+                            <!-- <p>Construida para acolher e educar as crianças de orfãs ...</p> -->
+                            <div class="card-button">
+                               <x-button.read-more>Saber +</x-button.read-more>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap w-full h-full md:w-1/4 shadow-xl">
+                        <div class="w-full h-2/3">
+                            <img class="object-cover w-full h-full" src="img/ProjectImg/tabpequinitos.jpg" alt="CEET" />
+                        </div>
+                        <div class="flex flex-wrap w-full h-1/3 p-1">
+                            <h2 class="card-title text-base text-white/70">Centro de Educação Especial e Terapêutica</h2>
+                            <!-- <p> Um espaço dedicado ao atendimento, tratamento e ...</p> -->
+                            <div class="card-button">
+                               <x-button.read-more>Saber +</x-button.read-more>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-wrap w-full h-full md:w-1/4 shadow-xl">
+                        <div class="w-full h-2/3">
+                            <img class="object-cover w-full h-full" src="img/ProjectImg/hospital.jpeg" alt="Hospital" />
+                        </div>
+                        <div class="flex flex-wrap w-full h-1/3 p-1">
+                            <h2 class="card-title text-base text-white/70">Apoio ao Hospital de Catió</h2>
+                            <!-- <p>If a dog chews shoes whose shoes does he choose?</p> -->
+                            <div class="card-button">
+                               <x-button.read-more>Saber +</x-button.read-more>
+                            </div>
+                        </div>
+                    </div>
+               </div>
+            </div>
+        </div>
+    </div>
+</div>
+
