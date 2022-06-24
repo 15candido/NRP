36b4f17f4769ca5434c73ace6cc1975f49149198
candido <candido.da.silva.ca@gmail.com>
2022-06-24 14:57:22 +0100

    Atualização

--- resources/views/components/cards/ongoing.blade.php
+++ resources/views/components/cards/ongoing.blade.php
@@ -1,5 +1,5 @@
 
-<div class="grid grid-flow-row justify-items-center w-full h-auto lg:resize-h overflow-hidden
+<div class="grid grid-flow-row justify-items-center w-full h-auto lg:resize-h 2xl:h-auto overflow-hidden
 mb-10 bg-gradient-to-r from-[#4ca747] via-[#019393] to-[#0082b3]">
     <div class="flex flex-col max-w-[91.666667%] w-full h-full justify-center lg:max-w-[83.333333%] gap-4">
         <div class="flex flex-col h-full justify-center items-center md:grid md:grid-cols-4 gap-2 text-white">
@@ -17,51 +17,59 @@ mb-10 bg-gradient-to-r from-[#4ca747] via-[#019393] to-[#0082b3]">
             </div>
             <div class="col-span-3 w-full h-full">
                <div class="flex flex-col w-full h-full justify-center md:flex-row gap-2 opacit">
-                    <div class="flex flex-wrap w-full h-full md:w-1/4 shadow-xl">
-                        <div class="w-full h-2/3">
-                            <img class="object-cover w-full h-full" src="img/ProjectImg/educacao.jpg" alt="Educação" />
-                        </div>
-                        <div class="flex flex-wrap w-full h-1/3 p-1">
-                            <h2 class="card-title text-base text-white/70">A Educação é o Único Caminho</h2>
-                            <!-- <p>A educação é um fator essencial para ...</p> -->
-                            <div class="card-button">
-                               <x-button.read-more>Saber +</x-button.read-more>
+                    <div class="flex flex-col w-full h-full md:w-1/4 shadow-xl">
+                        <div class="flex flex-wrap  w-full h-full gap-2">
+                            <div class="w-full h-2/3">
+                                <img class="object-cover w-full h-full" src="img/ProjectImg/educacao.jpg" alt="Educação" />
+                            </div>
+                            <div class="flex flex-wrap w-full h-1/3 p-1">
+                                <h2 class="card-title text-base text-white/70 w-full">A Educação é o Único Caminho</h2>
+                                <!-- <p>A educação é um fator essencial para ...</p> -->
+                                <div class="card-button">
+                                <x-button.read-more>Saber +</x-button.read-more>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="flex flex-wrap w-full h-full md:w-1/4 shadow-xl">
-                        <div class="w-full h-2/3">
-                            <img class="object-cover w-full h-full" src="img/ProjectImg/mame.jpeg" alt="Casa da Mamé" />
-                        </div>
-                        <div class="flex flex-wrap w-full h-1/3 p-1">
-                            <h2 class="card-title text-base text-white/70">Casa da Mamé - Acolhimento de Crianças</h2>
-                            <!-- <p>Construida para acolher e educar as crianças de orfãs ...</p> -->
-                            <div class="card-button">
-                               <x-button.read-more>Saber +</x-button.read-more>
+                    <div class="flex flex-col w-full h-full md:w-1/4 shadow-xl">
+                        <div class="flex flex-wrap w-full h-full gap-2">
+                            <div class="w-full h-2/3">
+                                <img class="object-cover w-full h-full" src="img/ProjectImg/mame.jpeg" alt="Casa da Mamé" />
+                            </div>
+                            <div class="flex flex-wrap w-full h-1/3 p-1">
+                                <h2 class="card-title text-base text-white/70 w-full">Casa da Mamé - Acolhimento de Crianças</h2>
+                                <!-- <p>Construida para acolher e educar as crianças de orfãs ...</p> -->
+                                <div class="card-button">
+                                <x-button.read-more>Saber +</x-button.read-more>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="flex flex-wrap w-full h-full md:w-1/4 shadow-xl">
-                        <div class="w-full h-2/3">
-                            <img class="object-cover w-full h-full" src="img/ProjectImg/tabpequinitos.jpg" alt="CEET" />
-                        </div>
-                        <div class="flex flex-wrap w-full h-1/3 p-1">
-                            <h2 class="card-title text-base text-white/70">Centro de Educação Especial e Terapêutica</h2>
-                            <!-- <p> Um espaço dedicado ao atendimento, tratamento e ...</p> -->
-                            <div class="card-button">
-                               <x-button.read-more>Saber +</x-button.read-more>
+                    <div class="flex flex-col w-full h-full md:w-1/4 shadow-xl">
+                        <div class="flex flex-wrap w-full h-full gap-2">
+                            <div class="w-full h-2/3">
+                                <img class="object-cover w-full h-full" src="img/ProjectImg/tabpequinitos.jpg" alt="CEET" />
+                            </div>
+                            <div class="flex flex-wrap w-full h-1/3 p-1">
+                                <h2 class="card-title text-base text-white/70 w-full">Centro de Educação Especial e Terapêutica</h2>
+                                <!-- <p> Um espaço dedicado ao atendimento, tratamento e ...</p> -->
+                                <div class="card-button">
+                                <x-button.read-more>Saber +</x-button.read-more>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="flex flex-wrap w-full h-full md:w-1/4 shadow-xl">
-                        <div class="w-full h-2/3">
-                            <img class="object-cover w-full h-full" src="img/ProjectImg/hospital.jpeg" alt="Hospital" />
-                        </div>
-                        <div class="flex flex-wrap w-full h-1/3 p-1">
-                            <h2 class="card-title text-base text-white/70">Apoio ao Hospital de Catió</h2>
-                            <!-- <p>If a dog chews shoes whose shoes does he choose?</p> -->
-                            <div class="card-button">
-                               <x-button.read-more>Saber +</x-button.read-more>
+                    <div class="flex flex-col w-full h-full md:w-1/4 shadow-xl">
+                        <div class="flex flex-wrap w-full h-full gap-2">
+                            <div class="w-full h-2/3">
+                                <img class="object-cover w-full h-full" src="img/ProjectImg/hospital.jpeg" alt="Hospital" />
+                            </div>                       
+                            <div class="flex flex-wrap w-full h-1/3 p-1">
+                                <h2 class="card-title text-base text-white/70 w-full">Apoio ao Hospital Regional de Tombali</h2>
+                                <!-- <p>If a dog chews shoes whose shoes does he choose?</p> -->
+                                <div class="card-button">
+                                <x-button.read-more>Saber +</x-button.read-more>
+                                </div>
                             </div>
                         </div>
                     </div>
