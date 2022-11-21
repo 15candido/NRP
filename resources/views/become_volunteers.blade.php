3b38d79eca4495e6aed0ea9d0c46729bb2425b10
candido.da.silva.ca@gmail.com <candido.da.silva.ca@gmail.com>
2022-11-21 17:46:13 +0000

    Atualização da pagina ser voluntário

--- resources/views/become_volunteers.blade.php
+++ resources/views/become_volunteers.blade.php
@@ -6,28 +6,39 @@
             alt="Voluntários da Na Rota dos Povos"
         /> <!--about us hero image -->
         <x-cards.structureWhitOutBg>
-            <div class="flex flex-col justify-start items-start py-20 gap-6">
-                <div class="full flex flex-col lg:flex-row gap-8">
+            <div class="flex flex-col lg:flex-row justify-start items-start py-20 gap-10">
+                <div class="w-full flex flex-col gap-8">
                     <x-cards.info-default-card
                         title="Voluntariado"
-                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, in architecto illum exercitationem 
-                        quos iusto enim unde commodi rerum sequi nulla cumque vero aliquam voluptatum eaque blanditiis alias, animi 
-                        necessitatibus!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, in architecto illum exercitationem 
-                        quos iusto enim unde commodi rerum sequi nulla cumque vero aliquam voluptatum eaque blanditiis alias, animi 
-                        necessitatibus!"
+                        description="O voluntariado assenta numa relação de solidariedade, mas é mais do que ter “boa vontade” e 
+                        “querer ajudar o outro”. É uma forma de cidadania ativa, mas que exige responsabilidade, compromisso e 
+                        respeito pela missão, cultura, e valores da entidade acolhedora. Aceitar a um projeto de voluntariado 
+                        de forma consciente e refletida implica:  
+                        - Conhecer e partilhar da missão e valores da organização;
+                        - Aceitar e cumprir as regras organizacionais e o mandato que lhe for atribuído no âmbito do 
+                        voluntariado; 
+                        - Ter a capacidade para trabalhar em equipa respeitando as hierarquias existentes;
+                        -Ter a humildade para descobrir os seus próprios preconceitos e aprender a “desmontá-las” de modo 
+                        a respeitar aqueles com quem e para quem trabalhamos e contexto social e cultural onde atuamos; 
+                        - Apresentar as condições físicas e psicológicas necessárias para o tipo de voluntariado proposto; 
+"
                     />
                     <x-cards.info-default-card
                         title="E ser o nosso voluntário?"
-                        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, in architecto illum exercitationem 
-                        quos iusto enim unde commodi rerum sequi nulla cumque vero aliquam voluptatum eaque blanditiis alias, animi 
-                        necessitatibus!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, in architecto illum exercitationem 
-                        quos iusto enim unde commodi rerum sequi nulla cumque vero aliquam voluptatum eaque blanditiis alias, animi 
-                        necessitatibus!"
+                        description="Ao tornar-se voluntário da Na Rota dos Povos, está a trabalhar para o desenvolvimento num 
+                        contexto social e cultural diferente, o que lhe obriga a sair da sua zona de conforto e a enfrentar com 
+                        pragmatismo os desafios do dia-a-dia em que a carência permanente é uma realidade. Desta forma, contribui
+                        para aquilo que é o nosso objetivo: a melhoria das condições de vida das populações junto das quais 
+                        atuamos, nas áreas da educação, da saúde, da sobrevivência, da proteção e do ambiente.
+
+                        O que oferecemos é uma oportunidade única de contribuir para o desenvolvimento de projetos que procuram 
+                        “fazer a diferença” numa região da Guiné-Bissau profundamente carenciada e em áreas consideradas chave 
+                        para a melhoria das condições de vida das populações; ganha-se uma experiência que, apesar de exigente, 
+                        acreditamos que será gratificante e educadora, bem como reconhecida através de um certificado de 
+                        Voluntariado."
                     />
                 </div>
-                <div class="relative w-full h-full">
+                <div class="relative w-full h-full rounded-xl shadow-lg p-4 hover:shadow-xl">
                     <x-volunteers.form/>
                 </div>
             </div>
--- resources/views/components/volunteers/form.blade.php
+++ resources/views/components/volunteers/form.blade.php
@@ -1,65 +1,39 @@
-<form action="" class="w-full space-y-4">
+<form action="" class="w-full space-y-4 ">
 <!-- rounded-xl shadow-lg p-4 hover:shadow-xl -->
+    <h2>Inscrever-me</h2>
     <div class="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0">
         <div class="w-full space-y-2">
             <label for="" class="block font-bold">Nome</label>
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
+            <input type="text" class="appearance-none block w-full bg-white ring-0 ring-gray-50 rounded-lg px-4 py-2 
             focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Nome">
         </div>
         <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Apelido</label>
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Nome">
+            <label for="" class="block font-bold">Data de Nascimento</label>
+            <input type="date" class="appearance-none block w-full bg-white ring-0 ring-gray-50 rounded-lg px-4 py-2 
+            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Data de Nascimento">
         </div>
     </div>
     <div class="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0">
         <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Email</label>
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Email">
+            <label for="" class="block font-bold">Telefone</label>
+            <input type="tel" class="appearance-none block w-full bg-white ring-0 ring-gray-50 rounded-lg px-4 py-2 
+            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Telefone">
         </div>
         <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Confirmar email</label>
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Confirmar email">
-        </div>
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Telemóvel</label>
-            <input type="tel" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Telemóvel">
-        </div>
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Em casa de emergência contactar?</label> 
-            <input type="tel" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Telemóvel">
-        </div>
+        <label for="" class="block font-bold">Email</label>
+        <input type="text" class="appearance-none block w-full bg-white ring-0 ring-gray-50 rounded-lg px-4 py-2 
+        focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Email">
+    </div>
+    </div>
+    <div class="w-full space-y-2">
+        <label for="" class="block font-bold">Morada</label>
+        <input type="text" class="appearance-none block w-full bg-white ring-0 ring-gray-50 rounded-lg px-4 py-2 
+        focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Morada">
     </div>
     <div class="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0">
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Data de nascimento</label>
-            <input type="date" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Data de nascimento">
-        </div>
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Naturalidade</label>
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Naturalidade">
-        </div>
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Nacionalidade</label> 
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Nacionalidade">
-        </div>
-    </div>
-    <div class="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0">
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Morada</label>
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Morada">
-        </div>
         <div class="relative w-full space-y-2">
             <label for="" class="block font-bold">Localidade</label>
-            <select class="block appearance-none w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
+            <select class="block appearance-none w-full bg-white ring-0 ring-gray-50 rounded-lg px-4 py-2 
             focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]">
                 <option>Selecionar</option>
                 <option>Aveiro</option>
@@ -78,90 +52,37 @@
         </div>
         <div class="w-full space-y-2">
             <label for="" class="block font-bold">Código Postal</label> 
-            <input type="number" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
+            <input type="number" class="appearance-none block w-full bg-white ring-0 ring-gray-50 rounded-lg px-4 py-2 
             focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Código postal">
-        </div>
-    </div>
-    <div class="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0">
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Habilitação</label>
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Habilitação">
-        </div>
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Profissão</label> 
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Profissão">
-        </div>
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">NIF</label> 
-            <input type="number" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="NIF">
-        </div>
-    </div>
-    <div class="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0">
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">BI/CC</label>
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="BI/CC">
-        </div>
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Validade</label> 
-            <input type="date" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Validade">
-        </div>
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Passaporte</label>
-            <input type="text" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="BI/CC">
-        </div>
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Validade</label> 
-            <input type="date" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" placeholder="Validade">
-        </div>
-    </div>
-    <div class="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0">
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Experiência como voluntário</label> 
-            <textarea name="" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" id="" cols="30" rows="6" placeholder="Experiência como voluntária">
-            </textarea>
-        </div>
-        <div class="w-full space-y-2">
-            <label for="" class="block font-bold">Experiência profissional</label> 
-            <textarea name="" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
-            focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" id="" cols="30" rows="6" placeholder="Experiência como voluntária">
-            </textarea>
         </div>
     </div>
     <div class="w-full space-y-2">
         <label for="" class="block font-bold">Motivação</label> 
-        <textarea name="" class="appearance-none block w-full bg-gray-200 ring-0 ring-gray-50 rounded-lg px-4 py-2 
+        <textarea name="" class="appearance-none block w-full bg-white ring-0 ring-gray-50 rounded-lg px-4 py-2 
         focus:outline-none focus:bg-white focus:ring-1 focus:ring-[#25caca]" id="" cols="30" rows="6" placeholder="De forma sucinta, 
         diga-nos as principais motivações para ser voluntario da Na Rota dos Povos">
         </textarea>
     </div>
-    <div class="flex flex-col md:flex-row gap-6 justify-center items-start">
-        <div class="space-y-4">
-            <h3>Documento Ser Voluntário</h3>
+    <div class="flex flex-col lg:flex-row gap-4 justify-center items-start text-xs">
+        <div class="space-y-3">
+            <h5 class="font-bold">Documento</h5>
             <div class="flex gap-4 justify-center items-center">
-                <input type="checkbox" class="">
-                <p>Recebi e tomei conhecimento do documento "Ser Voluntário da ONGD, Na Rota dos Povos"</p>
+                <input type="checkbox" class=" w-3 h-3">
+                <p>Recebi e tomei conhecimento do documento "ser voluntário da ONGD, Na Rota dos Povos"</p>
             </div>
         </div>
-        <div class="space-y-4">
-            <h3>Dossier</h3>
+        <div class="space-y-3">
+            <h5 class="font-bold">Dossier</h5>
             <div class="flex gap-4 justify-center items-center">
-                <input type="checkbox" class="">
-                <p>Recebi e tomei conhecimento do dossier "Projeros da ONGD, Na Rota dos Povos"</p>
+                <input type="checkbox" class=" w-3 h-3">
+                <p>Recebi e tomei conhecimento do dossier "projeros da ONGD, Na Rota dos Povos"</p>
             </div>
         </div>
-        <div class="space-y-4">
-            <h3>Consulta</h3>
+        <div class="space-y-3">
+            <h5 class="font-bold">Consulta</h5>
             <div class="flex gap-4 justify-center items-center">
-                <input type="checkbox" class="">
-                <p>Já frequentei a consulta do Viajante/ou fiz as vacinas obritórias</p>
+                <input type="checkbox" class=" w-3 h-3">
+                <p>Já frequentei a consulta do viajante/ou fiz as vacinas obritórias</p>
             </div>
         </div>
     </div>
