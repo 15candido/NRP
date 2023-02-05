abb3fdd432dc896b74f9d02648c501b6220a7591
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-02-05 11:37:30 +0000

    Title setting to left.

--- resources/views/components/about/about.blade.php
+++ resources/views/components/about/about.blade.php
@@ -1,10 +1,8 @@
 @foreach($about as $about)
-    <div class="flex flex-col items-center">
-        <span class="flex flex-col gap-2 p-2 justify-center text-justify leading-normal">
-            <h2 class="text-2xl md:text-3xl font-extrabold text-[#264653]">{{$about->title}}</h2>
-            <p>{{$about->description}}</p>
-        </span> 
-    </div>
+<div class="flex flex-col items-center">
+    <span class="flex flex-col gap-2 p-2 justify-center text-justify leading-normal">
+        <h2 class="text-left text-2xl md:text-3xl font-extrabold text-[#264653]">{{$about->title}}</h2>
+        <p>{{$about->description}}</p>
+    </span>
+</div>
 @endforeach
-
-
