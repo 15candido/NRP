83f1e44ce4b2bccfc5b813f173828d5efae22ab9
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-02-05 19:47:01 +0000

    On hover color was added.

--- resources/views/components/users/profile.blade.php
+++ resources/views/components/users/profile.blade.php
@@ -2,9 +2,13 @@
     <a href="#" class="pointer-events-none">
         <img class="pointer-events-auto hover:opacity-75" src="{{$profile->picture}}" alt="">
     </a>
-    <div class="p-4 space-y-2 hover:bg-gray-100">
-        <h4><a href="#" class="border-b-[1px] border-[#0082b3] border-opacity-75 hover:border-b-4 
-            hover:border-opacity-100 transition-all duration-300 ease-in-out">{{$profile->name}}</a></h4>
+    <div class="flex flex-col w-full items-center p-4 space-y-2 hover:bg-gray-100">
+        <h4 class="w-auto">
+            <a href="#" class="border-b-[1px] border-[#0082b3] border-opacity-75 hover:border-b-4 
+            hover:border-opacity-100 transition-all duration-300 ease-in-out">
+                {{$profile->name}}
+            </a>
+        </h4>
         <div>
             @if($profile->role)
             <p class="font-black">{{$profile->role}}</p>
