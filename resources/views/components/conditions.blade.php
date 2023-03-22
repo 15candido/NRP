73fa033eb37c55c6f317c692cfc5cf91db1ce5de
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-03-22 10:29:10 +0000

    V22_03_2023 - condiction component space ajustment.

--- resources/views/components/conditions.blade.php
+++ resources/views/components/conditions.blade.php
@@ -1,25 +1,25 @@
 <div class="flex flex-col space-y-4">
-    <div class="flex lg:p-4 gap-4 border-2 border-solid border-white hover:border-[#5ba057] rounded-lg">
+    <div class="flex gap-4 hover:px-4 lg:py-2 border-2 border-solid border-white hover:border-[#5ba057] rounded-lg">
         <input type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500">
         <label class="flex flex-col space-y-2">
-            <h5 class="font-black">Documento</h5>
-            <p>Recebi e tomei conhecimento do documento "ser voluntário da ONGD, Na Rota dos Povos"</p>
+            <h5 class="font-black mb-0">Documento</h5>
+            <p class="">Recebi e tomei conhecimento do documento "ser voluntário da ONGD, Na Rota dos Povos"</p>
         </label>
     </div>
 
-    <div class="flex lg:p-4 gap-4 border-2 border-solid border-white hover:border-[#5ba057] rounded-lg">
+    <div class="flex gap-4 hover:px-4 lg:py-2 border-2 border-solid border-white hover:border-[#5ba057] rounded-lg">
         <input type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500">
         <label class="flex flex-col space-y-2">
-            <h5 class="font-black">Dossier</h5>
-            <p>Recebi e tomei conhecimento do dossier projetos da ONGD, "Na Rota dos Povos"</p>
+            <h5 class="font-black mb-0">Dossier</h5>
+            <p class="">Recebi e tomei conhecimento do dossier projetos da ONGD, "Na Rota dos Povos"</p>
         </label>
     </div>
 
-    <div class="flex lg:p-4 gap-4 border-2 border-solid border-white hover:border-[#5ba057] rounded-lg">
+    <div class="flex gap-4 hover:px-4 lg:py-2 border-2 border-solid border-white hover:border-[#5ba057] rounded-lg">
         <input type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500">
         <label class="flex flex-col space-y-2">
-            <h5 class="font-black">Consulta</h5>
-            <p>Já frequentei a consulta do viajante/ou fiz as vacinas obritórias</p>
+            <h5 class="font-black mb-0">Consulta</h5>
+            <p class="">Já frequentei a consulta do viajante/ou fiz as vacinas obritórias</p>
         </label>
     </div>
 </div>
