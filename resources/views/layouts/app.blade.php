4f4cf9b97f98736a599d1c4a75ce1d4e6bec270c
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-04-11 12:33:22 +0100

    V11_04_2023. Js file was added to app Layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,40 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <x-partials.head :title="$title ?? ' ' "/>
-    </head>
 
-    {{-- Body Section --}}
-    <body class="font-sans antialiased">
-        {{-- Main Content --}}
-        <div class="flex min-h-screen bg-gray-300" id="wrapper" 
-        x-data="{ open: false }">
-            
-            {{-- Sidebar Section --}}
-            <div class="flex flex-col h-screen overflow-y-auto bg-white shadow transition-all 
+<head>
+    <x-partials.head :title="$title ?? ' ' " />
+</head>
+
+{{-- Body Section --}}
+
+<body class="font-sans antialiased">
+    {{-- Main Content --}}
+    <div class="flex min-h-screen bg-gray-300" id="wrapper" x-data="{ open: false }">
+
+        {{-- Sidebar Section --}}
+        <div class="flex flex-col h-screen overflow-y-auto bg-white shadow transition-all 
             duration-500" id="sidebar" :class="open ? 'w-96 md:w-64' : 'w-20'">
-                {{-- Side Bar Component --}}
-                <x-side-bar-menu-app />
-            </div>
-            
-            {{-- Body Content --}}
-            <div class="w-full h-screen overflow-y-auto transition-all duration-500">
-                {{-- App Header Component --}}
-                <x-app-header />
-
-                {{-- Main Section  --}}
-                <div class="px-6 py-3">
-                    <main class="block w-full min-h-full">
-                        {{$slot}}
-                    </main>
-                </div>
-            </div>
-
+            {{-- Side Bar Component --}}
+            <x-side-bar-menu-app />
         </div>
 
-        {{-- Scripts section --}}
-        @livewireScripts
-        @stack('scripts')
-        
-    </body>
+        {{-- Body Content --}}
+        <div class="w-full h-screen overflow-y-auto transition-all duration-500">
+            {{-- App Header Component --}}
+            <x-app-header />
+
+            {{-- Main Section  --}}
+            <div class="px-6 py-3">
+                <main class="block w-full min-h-full">
+                    {{$slot}}
+                </main>
+            </div>
+        </div>
+
+    </div>
+
+    {{-- Scripts section --}}
+    @livewireScripts
+    @stack('scripts')
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
 </html>
