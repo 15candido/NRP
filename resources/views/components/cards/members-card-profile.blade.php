5a997fef17d9fb57e14537803fed3aea43f15503
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-04-13 15:37:54 +0100

    V13_04_2023. Cursor-point and on hover border was removed from members-card-profile component.

--- resources/views/components/cards/members-card-profile.blade.php
+++ resources/views/components/cards/members-card-profile.blade.php
@@ -3,8 +3,7 @@
         <img src="{{asset($content->picture)}}" class="hover:opacity-75">
     </header>
     <div class="flex flex-col w-full items-center p-4 hover:bg-gray-100">
-        <strong href="#" class="uppercase border-b-[1px] border-solid border-[#0082b3] border-opacity-75 hover:border-b-4 
-            hover:border-opacity-100 transition-all duration-300 ease-in-out">
+        <strong href="#" class="uppercase border-b-[1px] border-solid border-[#0082b3] border-opacity-75">
             {{$content->name}}
         </strong>
         <div>
