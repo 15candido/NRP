50bfa502d6f52d19783bf11e5be01a0835954dd3
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-04-13 15:10:10 +0100

    V13_04_2023. Truncate class was removed in the ongoin project component.

--- resources/views/components/projects/ongoing.blade.php
+++ resources/views/components/projects/ongoing.blade.php
@@ -1,20 +1,15 @@
 <!-- Ongoi projects Card -->
 @foreach($projects as $project)
-<article class="flex w-full h-full justify-start items-center">
-    <div class="flex flex-col w-full h-full bg-white rounded-lg shadow-md justify-center md:max-w-md">
-        <a href="#" class="pointer-events-none">
-            <img src="{{$project->image}}" class="pointer-events-auto rounded-t-lg hover:opacity-75" alt="{{$project->alt}}">
+<article class="flex flex-col w-full h-full bg-white rounded-lg shadow-md justify-center md:max-w-md">
+    <a href="#" class="pointer-events-none">
+        <img src="{{$project->image}}" class="pointer-events-auto rounded-t-lg hover:opacity-75">
+    </a>
+    <div class="h-full p-4 rounded-lg hover:bg-gray-100 overflow-hidden">
+        <a href="#" class="inline-block mb-0">
+            <h4>{{$project->name}}</h4>
         </a>
-        <div class="w-full h-full space-y-4 p-4 rounded-lg hover:bg-gray-100">
-            <a href="#">
-                <h4 class=" md:truncate hover:break-words">
-                    {{$project->name}}
-                </h4>
-            </a>
-            <p class="text-base text-gray-600 break-words">{{ $project->short_description }}</p>
-            <x-buttons.button string="Saiba +" url="#" />
-        </div>
+        <p class="text-base text-gray-600 break-words">{{ $project->short_description }}</p>
+        <x-buttons.button string="Saiba +" url="#" />
     </div>
 </article>
-
 @endforeach
