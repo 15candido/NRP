86d185b950efbf46a23295cbf662d9cceac9aed0
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-06-01 23:11:01 +0100

    V01_06_2023. The hero2 was renaming to hero.

--- resources/views/components/homepage/hero.blade.php
+++ resources/views/components/homepage/hero.blade.php
@@ -1,49 +1,48 @@
-<section>
-    <div class="carousel">
-        <!-- Slide images  -->
-        <ul class="slides-wrapper">
-            <li class="slide active"><img src="images/heroes/1.jpg"></li>
-            <li class="slide">
-                <img src="images/heroes/2.jpg">
-            </li>
-            <li class="slide"><img src="images/heroes/3.jpg"></li>
-            <li class="slide"><img src="images/heroes/4.jpg"></li>
-            <li class="slide"><img src="images/heroes/5.jpg"></li>
-            <li class="slide"><img src="images/heroes/6.jpg"></li>
-        </ul>
+<div class="w-full mb-12">
+    <div class="relative h-[383px] sm:h-[433px] xl:h-[497px] 2xl:h-[520px] overflow-hidden">
+        <div x-data="{ visible: 0, images: ['/images/heroes/2.jpg', '/images/heroes/3.jpg', '/images/heroes/4.jpg' , '/images/heroes/5.jpg', '/images/heroes/6.jpg']}">
+            {{-- Slides images --}}
+            <template x-for="(image, index) in images">
+                <div class="absolute w-full h-full">
+                    <img class="object-cover object-center w-full h-full" :src="image" x-show="index == visible" />
+                </div>
+            </template>
 
-        <!-- Slide indicators  -->
-        <div class="slide-indicators">
-            <button type="button" class="active"></button>
-            <button type="button"></button>
-            <button type="button"></button>
-            <button type="button"></button>
-            <button type="button"></button>
-            <button type="button"></button>
+            {{-- Slides indicators --}}
+            <div class="absolute inline-block space-x-2 gap-3 bottom-8 left-1/2 -translate-x-1/2">
+                <template x-for="(image, index) in images">
+                    <button @click="visible = index" class="w-4 h-4 p-0 border-none rounded-full cursor-pointer outline-none
+                     bg-white/80 hover:bg-white shadow-sm shadow-gray-600"></button>
+                    <!-- <p @click="visible = index" class="rounded-full bg-white p-4"></p> -->
+                </template>
+            </div>
+
+            {{-- Right navigation button --}}
+            <button @click="visible++; if (visible == images.length) visible = 0;" class="absolute flex 
+            items-center justify-center h-auto top-1/2 -translate-y-1/2 z-30 right-0 px-5 lg:right-40 lg:px-0 
+            pointer-events-none group focus:outline-none">
+                <span class="next-button enabled-button inline-flex items-center justify-center w-10 h-10 rounded-full bg-white 
+                    shadow-sm shadow-gray-400 hover:shadow-lg group-hover:bg-white/70 group-focus:ring-4
+                    group-focus:ring-white/90 group-focus:outline-none transition duration-300 ease-in-out">
+                    <svg class="w-6 h-6 text-gray-500 group-hover:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
+                    </svg>
+                    <span class="sr-only">Next</span>
+                </span>
+            </button>
+            {{-- Left navigation button --}}
+            <button @click="visible--; if (visible == -1) visible = 4;" class="absolute flex items-center 
+            justify-center h-auto top-1/2 -translate-y-1/2 z-30 left-0 px-5 lg:left-40 lg:px-0 
+            pointer-events-none group focus:outline-none">
+                <span class="prev-button  enabled-button inline-flex items-center justify-center w-10 h-10 rounded-full
+                bg-white shadow-sm shadow-gray-400 hover:shadow-lg group-hover:bg-white/70 group-focus:ring-4
+                group-focus:ring-white/90 group-focus:outline-none transition duration-300 ease-in-out">
+                    <svg class="w-6 h-6 text-gray-500 group-hover:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
+                    </svg>
+                    <span class="sr-only">Previous</span>
+                </span>
+            </button>
         </div>
-
-        <!-- Navigation buttons -->
-        <button type="button" class="absolute flex items-center justify-center h-auto top-1/2 -translate-y-1/2 z-30 left-0  
-        px-5 lg:left-40 lg:px-0 pointer-events-none group focus:outline-none">
-            <span class="prev-button inline-flex items-center justify-center w-10 h-10 rounded-full bg-white 
-            shadow-sm shadow-gray-400 hover:shadow-lg group-hover:bg-white/70 group-focus:ring-4 group-focus:ring-white/90 
-            group-focus:outline-none transition duration-300 ease-in-out">
-                <svg class="w-6 h-6 text-gray-500 group-hover:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
-                </svg>
-                <span class="sr-only">Previous</span>
-            </span>
-        </button>
-        <button type="button" class="absolute flex items-center justify-center h-auto top-1/2 -translate-y-1/2 z-30 right-0 
-        px-5 lg:right-40 lg:px-0 pointer-events-none group focus:outline-none">
-            <span class="next-button enabled-button inline-flex items-center justify-center w-10 h-10 rounded-full bg-white 
-            shadow-sm shadow-gray-400 hover:shadow-lg group-hover:bg-white/70 group-focus:ring-4 group-focus:ring-white/90 
-            group-focus:outline-none transition duration-300 ease-in-out">
-                <svg class="w-6 h-6 text-gray-500 group-hover:text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-                </svg>
-                <span class="sr-only">Next</span>
-            </span>
-        </button>
     </div>
-</section>
+</div>
