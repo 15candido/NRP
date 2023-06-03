0912bb98268791c616bd869d7fc3ddbfa5fefce2
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-06-03 21:51:06 +0100

    V03_06_2023_21h49. The register/login was added on Nav

--- resources/views/components/partials/nav.blade.php
+++ resources/views/components/partials/nav.blade.php
@@ -1,11 +1,4 @@
 <header class="flex flex-wrap justify-center items-center mb-6">
-    @if (Auth::user())
-        <form method="POST" action="/logout">
-            @csrf
-            <div>Olá {{ Auth::user()->name }}</div>
-            <button class="bg-red-400 text-white px-2 py-1">Logout</button>
-        </form>
-    @endif
     <div class="relative fixed-width mt-8 pb-5">
         {{-- Logo --}}
         <div class="absolute flex flex-row gap-4 items-center top-1/2 -translate-y-1/2">
@@ -47,6 +40,32 @@
                         <span class="text-xs font-bold text-gray-600 uppercase">Contacto</span>
                     </a>
                 </li>
+
+                {{-- Login/Registration --}}
+                @if (Auth::user())
+                <li class="flex justify-center items-center p-0 m-0">
+                    <form method="POST" action="/logout" class="flex flex-row gap-2 justify-between">
+                        @csrf
+                        <div class="flex flex-row justify-between items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-8 h-8 
+                            stroke-current stroke-[2px]">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
+                            </svg>
+                            <h5 class="text-xs font-bold text-gray-600 uppercase">Olá {{ Auth::user()->name }}</h5>
+                        </div>
+                        <button class="bg-red-400 uppercase text-white px-2">Logout</button>
+                    </form>
+                </li>
+                @else
+                <a href="#" class="flex flex-row justify-between items-center opacity-50 hover:opacity-100 ">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 h-8 fill-none stroke-[2px] 
+                    stroke-current p-1.5 opacity-50 hover:opacity-100 rounded-full">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 
+                        017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                    </svg>
+                    <span class="text-xs font-bold text-gray-600 uppercase">Login</span>
+                </a>
+                @endif
             </ul>
         </div>
         {{-- Premary menu --}}
