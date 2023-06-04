b339689074d496bdedd2b4cbde8f5ba7210e952e
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-06-05 00:06:31 +0100

    V05_06_2023_00h04. The project image was  Reesizing.

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -1,7 +1,7 @@
 @props(['image' => null, 'name' => null])
 <a href="#" class="hover:scale-[1.01]">
     <article class="h-full border-solid shadow-md bg-white border-nrp-blue border-2 overflow-hidden rounded-xl">
-        <img src="{{ $image }}">
+        <img src="{{ $image }}" class="bg-red-400">
         <div class="px-4 py-2 rounded-lg">
             <p class="font-bold">{{ $name }}</p>
             <div>{!! $slot !!}</div>            
