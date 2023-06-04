5ec27af1c09f5d5cbc40d1124c7fa8b1c37c005e
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-06-04 09:54:19 +0100

    V04_06_2023_09h52. Reesizing project components.

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -1,8 +1,8 @@
 @props(['image' => null, 'name' => null])
 <a href="#" class="hover:scale-[1.01]">
-    <article class="bg-white border-solid shadow-lg border-gray-300 border-2 overflow-hidden rounded-xl ">
+    <article class="h-full border-solid shadow-lg border-gray-300 border-2 overflow-hidden rounded-xl ">
         <img src="{{ $image }}">
-        <div class="px-4 py-2 rounded-lg">        
+        <div class="px-4 py-2 rounded-lg">
             <p class="font-bold">{{ $name }}</p>
             <div>{!! $slot !!}</div>
         </div>
