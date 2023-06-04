f28a7482a49bbb27855326d2327e8e90fbfc5bd1
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-06-05 00:29:11 +0100

    V05_06_2023_00h04. Fixed merge conflict.

--- resources/views/components/form/newsletter.blade.php
+++ resources/views/components/form/newsletter.blade.php
@@ -1,5 +1,5 @@
-<div class="flex flex-col gap-2 w-full h-auto justify-center items-center">
-    <p>
+<div class="flex flex-col gap-4 w-full h-auto justify-center items-center">
+    <p class="text-xs text-white/70 leading-5 hover:text-white">
         Abrace a nossa causa, oferecendo a sua energia, talento e
         recursos para levar a inspiração e esperança a quem precisa.
     </p>
