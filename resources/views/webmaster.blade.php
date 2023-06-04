ad0d7fb4c0c552e9235573a24efe5610e61fd097
Cândido Silva <candido.da.silva.ca@gmail.com>
2023-06-04 01:01:27 +0100

    V04_06_2023_01h00. Webmaster page

---
+++ resources/views/webmaster.blade.php
@@ -0,0 +1,26 @@
+<x-guestLayout>
+    <div class="w-full min-h-screen flex items-center">
+        <div class="w-full max-w-7xl m-auto flex flex-col md:flex-row justify-center items-center gap-12 px-5 lg:px-10">
+            <div class="w-full max-w-sm flex flex-col items-center py-10 rounded-lg shadow-lg">
+                <img src="images/user/david.jpg" class="w-24 h-24 mb-3 object-cover rounded-full shadow-lg" />
+                <h5 class="mb-1 text-sm md:text-base lg:text-lg font-bold text-gray-700">David Freitas</h5>
+                <span class="text-sm text-gray-500">Backend Developer</span>
+                <div class="flex mt-4 space-x-3 md:mt-6">
+                    <a href=""><img src="images/media/facebook.svg" class="w-5 h-5"></a>
+                    <a href=""><img src="images/media/instagram.svg" class="w-5 h-5"></a>
+                    <a href=""><img src="images/media/linkedin.svg" class="w-5 h-5"></a>
+                </div>
+            </div>
+            <div class="w-full max-w-sm flex flex-col items-center py-10 rounded-lg shadow-lg">
+                <img src="images/user/candido.jpg" class="w-24 h-24 mb-3 object-cover rounded-full shadow-lg" />
+                <h5 class="mb-1 text-sm md:text-base lg:text-lg font-bold text-gray-700">Cândido da Silva</h5>
+                <span class="text-sm text-gray-500">Frontend Developer</span>
+                <div class="flex mt-4 space-x-3 md:mt-6">
+                    <a href=""><img src="images/media/facebook.svg" class="w-5 h-5"></a>
+                    <a href=""><img src="images/media/instagram.svg" class="w-5 h-5"></a>
+                    <a href=""><img src="images/media/linkedin.svg" class="w-5 h-5"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-guestLayout>
